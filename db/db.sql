301ecc4edbb3022aea52a55bfa9c1f4a69a9dabd
liaobaidi <114607245@qq.com>
2022-12-05 22:25:42 +0800

    【update】修改sql语句

--- db/db.sql
+++ db/db.sql
@@ -9,7 +9,7 @@ create table if not exists `sys_user_liao`
 (
 `id` int not null auto_increment comment 'id' primary key,
 `username` varchar(16) not null comment '用户名',
-`account` varchar(16) not null comment '账号' primary key,
+`account` varchar(16) not null comment '账号',
 `psw` varchar(32) not null comment '密码',
 `class_id` int not null comment '班级id'
 ) comment '用户表';
